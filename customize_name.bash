7a351f250537236a836c3249be9f007e802c13ca
Adrian <adrian.kuhn@arcor.de>
2025-10-26 16:10:57 +0100

    feat(template): enhance project customization script
    - improve handling of package name replacements using string substitution
    - add functionality to create new directories for project structure
    - implement moving of source files into new directory paths
    - refine output messages for better readability

--- customize_name.bash
+++ customize_name.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CURRENT_DIR="$(pwd)"
 
@@ -10,11 +11,32 @@ CYAN='\033[0;36m'
 RESET='\033[0m'
 
 OLD_PROJECT_NAME="a-trade-microservice-""template" # this would be replaced otherwise
-PROJECT_NAME="$(basename "$CURRENT_DIR")"
-OLD_PROJECT_NAME_PACKAGE="$(echo "$OLD_PROJECT_NAME" | sed "s|-|.|g")"
-PROJECT_NAME_PACKAGE="$(echo "$PROJECT_NAME" | sed "s|-|.|g")"
+PROJECT_NAME="$(basename "$SCRIPT_DIR")"
 
-find . -type f -exec sed -i "s|$OLD_PROJECT_NAME_PACKAGE|$PROJECT_NAME_PACKAGE|g" {} \;
+OLD_PROJECT_NAME_PACKAGE="${OLD_PROJECT_NAME//-/.}"
+PROJECT_NAME_PACKAGE="${PROJECT_NAME//-/.}"
 
+OLD_PROJECT_NAME_PATH="${OLD_PROJECT_NAME//-/\/}/"
+PROJECT_NAME_PATH="${PROJECT_NAME//-/\/}/"
+
+cd "$SCRIPT_DIR"
+
+echo -e "${CYAN}-->Replacing package names${RESET}"
+find ./src -type f -exec sed -i "s|$OLD_PROJECT_NAME_PACKAGE|$PROJECT_NAME_PACKAGE|g" {} \;
+
+echo -e "${CYAN}-->Creating directories${RESET}"
+mkdir -p "./src/main/kotlin/$PROJECT_NAME_PATH"
+mkdir -p "./src/test/kotlin/$PROJECT_NAME_PATH"
+
+echo -e "${CYAN}-->Moving source files${RESET}"
+if [ -e "./src/main/kotlin/$OLD_PROJECT_NAME_PATH" ]; then
+  mv -f "./src/main/kotlin/$OLD_PROJECT_NAME_PATH" "./src/main/kotlin/$PROJECT_NAME_PATH"
+fi
+if [ -e "./src/test/kotlin/$OLD_PROJECT_NAME_PATH" ]; then
+  mv -f "./src/test/kotlin/$OLD_PROJECT_NAME_PATH" "./src/test/kotlin/$PROJECT_NAME_PATH"
+fi
+
+echo -e "${CYAN}-->Replacing project names in all files...${RESET}"
 find . -type f -exec sed -i "s|$OLD_PROJECT_NAME|$PROJECT_NAME|g" {} \;
 
+echo -e "${GREEN}Project customization completed successfully!${RESET}"
